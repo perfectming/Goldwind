834e66127c0a239670e1f0333ff333a4f13377fd
perfectming <752753032@qq.com>
2016-11-10 10:32:55 +0800

    11-10-1

--- app/routes/pages/mainPage/comp/Fan.jsx
+++ app/routes/pages/mainPage/comp/Fan.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Corner from './super/Corner.jsx';
+import Block from './super/block.jsx';
+import Title from './super/Title.jsx';
+import Column from './chart/Column.jsx';
+import Pie from './chart/Pie.jsx';
+import Line from './chart/Line.jsx';
 import styles from './Fan.scss';
 let fanData = require('../../../../../config/fan-data');
 
@@ -12,8 +18,54 @@ let Component = React.createClass({
 
     render() {
         return (
-            <div className={styles.listbodyBox}>
-                {
+            <div className={styles.bodyBox}>
+                <div className={styles.leftBox}>
+                    <div className={styles.states}>
+                         <Corner></Corner>
+                         <Title title={['风场指标','综合指标']}></Title>
+                         <div className={styles.wind}>
+                            <span className={styles.num}><a className={styles.anum1}>风场个数</a><a className={styles.anum}><b>3</b>个</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>风机台数</a><a className={styles.anum}><b>140</b>台</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>平均风速</a><a className={styles.anum}><b>3.31</b>m/s</a></span>
+                            <span className={styles.gfzb}>光伏指标</span>
+                            <span className={styles.num}><a className={styles.anum1}>电厂个数</a><a className={styles.anum}><b>3.31</b>个</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>逆变器</a><a className={styles.anum}><b>3.31</b>台</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>辐照度</a><a className={styles.anum}><b>3.31</b>m/s</a></span>
+                         </div>
+                          <div className={styles.wind}>
+                            <span className={styles.num}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>3</b>个</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>装机容量</a><a className={styles.anum}><b>140</b>台</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>日发电量</a><a className={styles.anum}><b>3.31</b>m/s</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>月发电量</a><a className={styles.anum}><b>3.31</b>个</a></span>
+                            <span className={styles.num}><a className={styles.anum1}>年发电量</a><a className={styles.anum}><b>3.31</b>台</a></span>
+                            
+                         </div>
+                    </div>
+                    <div className={styles.Speed}>
+                        <Corner></Corner>
+                        <Title title={['机组运行状态','逆变器运行状态']}></Title>
+                        <div className={styles.spedc}>
+                            <Block></Block>
+                        </div>
+                        <div className={styles.totalbox}>
+                           总计<a>268</a>台
+                        </div>
+                        
+                    </div>
+                     <div className={styles.Situation}>
+                        <Corner></Corner>
+                        <Title title={['发电量完成情况']}></Title>
+                        
+                    </div>
+                    <div className={styles.Completion}>
+                       <Corner></Corner>
+                       <Title title={['发电量完成率']}></Title>
+                       <Column></Column>
+                       
+                    </div>
+                </div>
+                <div className={styles.listbodyBox}>
+                    {
                     fanData.data.area.map((value, key)=> {
                         return (
                             <div className={styles.listheaderBox} key={key}>
@@ -54,7 +106,8 @@ let Component = React.createClass({
                         
                     })
                    
-                }
+                    }
+                </div>
             </div>
         );
     }
