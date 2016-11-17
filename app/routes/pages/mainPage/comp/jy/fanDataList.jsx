99e5d418d555a0df98aebc17252f9c82f2a429ff
QieT <644684987@qq.com>
2016-11-17 20:48:15 +0800

    111

--- app/routes/pages/mainPage/comp/jy/fanDataList.jsx
+++ app/routes/pages/mainPage/comp/jy/fanDataList.jsx
@@ -1,77 +1,67 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import css from  './Tkgl.scss'
+import css from  './fanDataList.scss'
 var actions = require('redux/actions');
 let database = require('./data');
-let data=database.fanDataList.content;
+let data=database.fanDataList;
 var page=1;
-let run=0;
-let fau=0;
-let offL=0;
-let stand=0;
-let haul=0;
-for(let i=0;i<data.length;i++){
-    switch (data[i][4]%5){
+var run=0;
+var fau=0;
+var offL=0;
+var stand=0;
+var haul=0;
+for(let i=0;i<data.content.length;i++){
+    switch ((data.content[i][5])%5){
         case 0:
             run++;
-            return;
+            break;
         case 1:
             fau++;
-            return;
+            break;
         case 2:
             offL++;
-            return;
+            break;
         case 3:
             stand++;
-            return;
+            break;
         case 4:
             haul++;
-            return;
+            break;
     }
-}console.log(page,run,fau,offL,stand,haul);
+}
 let Component = React.createClass({
     componentDidMount() {
         this.props.init();
     },
 
     render() {
+        let {next,previous} = this.props;
         return (
-            <div className={css.tkglBox}>
-                <button onClick={page>1?page--:page}>上一页</button>{page}/{Math.ceil(data.length/26)}
-                <button onClick={page<data.length/26?page++:page}>下一页</button>
+            <div className={css.toBox}>
                 <div className={css.leftBox}>
-                    <div className={styles.tableBox}>
-                        <div className={styles.tableHeaderBox}>
+                    <div className={css.tableBox}>
+                        <div className={css.tableHeaderBox}>
                             {
-                                data.fanDataList.header.map((value, key)=> {
+                                data.header.map((value, key)=> {
                                     return (
-                                        <div className={styles.tableHeaderItem}
-                                             style={{width:(100/data.fanDataList.header.length)+"%"}} key={key}>{value}</div>
+                                        <div className={css.tableHeaderItem}
+                                             style={{width:(100/data.header.length)+"%"}} key={key}>{value}</div>
                                     )
                                 })
                             }
                         </div>
-                        <div className={styles.tableContentBox}>
+                        <div className={css.tableContentBox}>
                             {
-                                data.map((value, key)=> {
-                                    if(key<page*13){
+                                data.content.map((value, key)=> {
+                                    if(13*(page-1)<=key&&key<page*13){
                                     return (
-                                        <div className={key%2===0? styles.tableContentLine : styles.tableContentLine1} key={key}>
+                                        <div className={key%2===0? css.tableContentLine : css.tableContentLine1} key={key}>
                                             {
                                                 value.map((valueC, keyC)=> {
-                                                    if(keyC==value.length-1){
                                                         return (
-                                                            <input className={styles.tableContentItem}
-                                                                   style={{width:(100/data.fanDataList.header.length)+"%"}}
-                                                                   key={keyC} contentEditable="true"
-                                                                   onChange={(e)=>changeTableItem(e.target.value,table,key,keyC)}
-                                                                   value={valueC}/>
-                                                        )
-                                                    }else{
-                                                        return (
-                                                            <div className={styles.tableContentItem} style={{width:(100/data.fanDataList.header.length)+"%"}}
+                                                            <div className={css.tableContentItem} style={{width:(100/data.header.length)+"%"}}
                                                                  key={keyC}>{valueC}</div>
-                                                        );}
+                                                        );
                                                 })
                                             }
                                         </div>
@@ -82,38 +72,29 @@ let Component = React.createClass({
                     </div>
                 </div>
                 <div className={css.rightBox}>
-                    <div className={styles.tableBox}>
-                        <div className={styles.tableHeaderBox}>
+                    <div className={css.tableBox}>
+                        <div className={css.tableHeaderBox}>
                             {
-                                data.fanDataList.header.map((value, key)=> {
+                                data.header.map((value, key)=> {
                                     return (
-                                        <div className={styles.tableHeaderItem}
-                                             style={{width:(100/data.fanDataList.header.length)+"%"}} key={key}>{value}</div>
+                                        <div className={css.tableHeaderItem}
+                                             style={{width:(100/data.header.length)+"%"}} key={key}>{value}</div>
                                     )
                                 })
                             }
                         </div>
-                        <div className={styles.tableContentBox}>
+                        <div className={css.tableContentBox}>
                             {
-                                data.map((value, key)=> {
+                                data.content.map((value, key)=> {
                                     if(key>=page*13&&key<page*26){
                                     return (
-                                        <div className={key%2===0? styles.tableContentLine : styles.tableContentLine1} key={key}>
+                                        <div className={key%2===0? css.tableContentLine1 : css.tableContentLine} key={key}>
                                             {
                                                 value.map((valueC, keyC)=> {
-                                                    if(keyC==value.length-1){
                                                         return (
-                                                            <input className={styles.tableContentItem}
-                                                                   style={{width:(100/data.fanDataList.header.length)+"%"}}
-                                                                   key={keyC} contentEditable="true"
-                                                                   onChange={(e)=>changeTableItem(e.target.value,table,key,keyC)}
-                                                                   value={valueC}/>
-                                                        )
-                                                    }else{
-                                                        return (
-                                                            <div className={styles.tableContentItem} style={{width:(100/data.fanDataList.header.length)+"%"}}
+                                                            <div className={css.tableContentItem} style={{width:(100/data.header.length)+"%"}}
                                                                  key={keyC}>{valueC}</div>
-                                                        );}
+                                                        );
                                                 })
                                             }
                                         </div>
@@ -123,12 +104,15 @@ let Component = React.createClass({
                         </div>
                     </div>
                 </div>
-                <div className={styles.btnClass}>
-                    <button>运行</button>
-                    <button>故障</button>
-                    <button>离线</button>
-                    <button>待机</button>
-                    <button>检修</button></div>
+                <div className={css.btnClass}>
+                    <button className={css.run}>运行 &nbsp; {run}</button>
+                    <button className={css.fau}>故障 &nbsp; {fau}</button>
+                    <button className={css.offL}>离线 &nbsp; {offL}</button>
+                    <button className={css.stand}>待机 &nbsp; {stand}</button>
+                    <button className={css.haul}>检修 &nbsp; {haul}</button>
+                    <button className={css.btnP} onClick={previous}>上一页</button><span className={css.txt}>{page}/{Math.ceil(data.content.length/26)}</span>
+                    <button className={css.btnP} onClick={next}>下一页</button>
+                </div>
             </div>
         );
     }
@@ -146,6 +130,12 @@ const mapDispatchToProps = (dispatch) => {
                 test:''
             }
         },
+        previous:()=>{
+            page>1 ? page--:page;
+        },
+        next:()=>{
+            (page<(data.content.length/26)) ? page++:page;
+        }
     };
 };
 
