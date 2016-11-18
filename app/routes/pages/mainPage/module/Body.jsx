675e63a2ba0fc70dd3d1cf9f39b7a259a8ecf056
QieT <644684987@qq.com>
2016-11-18 14:00:02 +0800

    121

--- app/routes/pages/mainPage/module/Body.jsx
+++ app/routes/pages/mainPage/module/Body.jsx
@@ -64,7 +64,7 @@ let Component = React.createClass({
 });
 
 
-const mapStateToProps = (state) => {
+const mapStatemapStps = (state) => {
     return {
         showPage: state.vars.showPage,
         flag: state.vars.bodypage,
