b9a9e73bdba552a64659fad42ab97064c8aadcbc
QieT <644684987@qq.com>
2016-11-25 17:20:51 +0800

    123

--- app/routes/pages/mainPage/comp/super/navleft.jsx
+++ app/routes/pages/mainPage/comp/super/navleft.jsx
@@ -116,7 +116,9 @@ let Component = React.createClass({
                                 </div>
                             )
                         })
+
                     }
+
                 </div>
             </div>
         );
