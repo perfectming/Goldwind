08462878604a672c8e3d97eca5d76918d7b118a2
perfectming <752753032@qq.com>
2016-12-06 20:24:36 +0800

    ming120602

--- static/mainapi.js
+++ static/mainapi.js
@@ -208,7 +208,7 @@ TY.writeInfo = function (msg, flag, lock,wt)
 }
 TY.writeError = function (error)
 {
-    msg = 1;
+    window.status = msg;
     $(".error").text("error:"+error.msg+" 故障号:"+error.code);
 }
 
