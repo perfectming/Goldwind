cfb1038e766c0ffdcaffa719fbf2381ba81189a4
Max-in <610261393@qq.com>
2016-12-08 16:06:18 +0800

    maxxxx23

--- app/routes/pages/mainPage/comp/healthManagement/yujing.jsx
+++ app/routes/pages/mainPage/comp/healthManagement/yujing.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import styles from './main_style.scss';
-
-
+let timer;
 var actions = require('redux/actions');
 
 let Component = React.createClass({
-    componentDidMount() {
-        this.props.init();
+    componentWillMount() {
+        localStorage.setItem("keyone", 1);
+        localStorage.setItem("keytwo", 0);
     },
-
+    componentDidMount() {
+        let {changekey} = this.props;
+        this.props.init();
+        timer = setInterval(function(){
+            changekey()
+        },200)
+    },
+    componentWillUnmount: function(){
+        clearInterval(timer);
+    },
     render() {
         return(
             <iframe className={styles.bodyBox} src="../../../static/sphm/yj_index.html" scrolling="no"></iframe>
@@ -28,6 +37,16 @@ const mapDispatchToProps = (dispatch) => {
         init: () => {
             var obj = {
                 test:''
+            }
+        },
+        changekey: () => {
+            dispatch(actions.setVars('treeItemActive', localStorage.keyone));
+            if( localStorage.keyone == 0 ){
+                dispatch(actions.setVars('navhide',false ));
+            }else {
+                // dispatch(actions.setVars('tabItem', true));
+                dispatch(actions.setVars('navhide', true));
+                dispatch(actions.setVars('tabItemActive', localStorage.keytwo));
             }
         },
     };
