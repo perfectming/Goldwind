ef91c369f969fd4a5371279bd5c027a348f0cd22
QieT <644684987@qq.com>
2016-12-21 17:44:34 +0800

    jy119

--- app/routes/pages/mainPage/Login.jsx
+++ app/routes/pages/mainPage/Login.jsx
@@ -8,7 +8,9 @@ let page = require('../../../../config/page');
 let comp = require('../../../../config/comp');
 import css from './Login.scss';
 require('jquery.cookie');
-let tabaleData = require('../../../../config/table-data');
+var codeChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 let Component = React.createClass({
   componentDidMount() {
@@ -16,14 +18,20 @@ let Component = React.createClass({
   },
   render() {
     let {login,}=this.props;
+    let code='';
+    for (let i=0;i<4;i++){
+      code+=codeChars[Math.floor(Math.random()*52)]
+    }
     return (
         <FixedContent mode="fullWidth" width={1920}>
           {
             <div className={css.whole}>
             <form className={css.loginBox}>
-              <input placeholder=" 用户名:" className={css.int} id="qq" type="text" name="username"/><br/>
-              <input placeholder=" 密码:" className={css.int} type="password" name="password"/><br/>
-              <input className={css.submit} type="submit " value='登      陆' readOnly="true" onClick={()=>login(123)}/>
+              <input placeholder=" 用户名:" className={css.int} id="username1" type="text" name="username"/><br/>
+              <input placeholder=" 密码:" className={css.int} id="password1" type="password" name="password"/><br/>
+              <input placeholder=" 验证码:" className={css.ints} id="check" type="text" name="check"/>
+              <input value={code} readOnly="readOnly" id="checked" className={css.pages}/><br/>
+              <input className={css.submit} type="submit " value='登      陆' readOnly="true" onClick={()=>login()}/>
             </form>
           </div>
           }
@@ -45,49 +53,39 @@ const mapDispatchToProps = (dispatch) => {
       $.cookie('token','123123');
        // browserHistory.push('/app/all/page/main')  ;
     },
-    login:(value)=>{
-      !value?
-      alert('请输入账号'):
-       browserHistory.push('/app/all/page/main')  ;
-      dispatch(actions.setVars('userInfo', value));
-      // login();
+    login:()=>{
+      $('#check')[0].value!==$('#checked')[0].value?
+      alert('验证码输入错误'):
+      browserHistory.push('/app/all/page/main')  ;
+      dispatch(actions.setVars('userInfo', true));
       try { Base.returnPlay(); } catch (e) { };
       try { if (TY == null) { } } catch (e) { alert("配置文件加载失败!"); return; }
-      //
       TY.dataUrl = "http://54.223.200.134/System/data.aspx";
       TY.crossDomain = true;
       TY.Zip =false;
       TY.TT.timeOutlength = 1000*60*1;
+          /*$.ajax({
+            url: 'http://10.9.0.16:9080/soam/user/login',
+            type: 'post',
+            data:'name='+$('#username1')[0].value+'&&password='+$('#password1')[0].value,
+            dataType: 'json',//here,
+            success:function (data) {
+              data.data.result==='False'?
+              alert('用户名或密码错误'):
+              browserHistory.push('/app/all/page/main')  ;
+              dispatch(actions.setVars('userInfo', true));
+              try { Base.returnPlay(); } catch (e) { };
+              try { if (TY == null) { } } catch (e) { alert("配置文件加载失败!"); return; }
+              TY.dataUrl = "http://54.223.200.134/System/data.aspx";
+              TY.crossDomain = true;
+              TY.Zip =false;
+              TY.TT.timeOutlength = 1000*60*1;
 
-
-      var st = null, et = null, th = null, re = null;
-
-   // function login() {
-      //   $.ajax({
-      //     url: "http://54.223.200.134/System/mlogin.aspx",
-      //     async: false,
-      //     data: 'loginType=4&P_username=bmjk&P_password=1',
-      //     type: "post",
-      //     dataType: "text",
-      //     complete: function () {
-      //
-      //     },
-      //     success: function (data, state) {
-      //       if (TY.Zip == true) {
-      //         eval("data=" + utf8to16(zip_inflate(base64decode(data))));
-      //       }
-      //       else {
-      //         eval("data=" + data);
-      //       }
-      //       if (data.f === false)
-      //         alert(data);
-      //     },
-      //     error: function (a, b, c) {
-      //       // alert(a + ":" + b + ":" + c);
-      //
-      //     }
-      //   });
-      // }
+            },
+            error:function(){
+              console.log('获取数据失败')
+            }
+          });*/
     }
   }
 };
