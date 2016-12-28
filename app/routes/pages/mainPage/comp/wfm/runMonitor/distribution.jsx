0703cecd7d23197c1311898ea5238959b5f3c5d4
a617321361 <1853412880@qq.com>
2016-12-28 13:53:04 +0800

    lin19

--- app/routes/pages/mainPage/comp/wfm/runMonitor/distribution.jsx
+++ app/routes/pages/mainPage/comp/wfm/runMonitor/distribution.jsx
@@ -22,10 +22,12 @@ let Component = React.createClass({
     },
 
     render() {
-        let {clickNumber, showFlag, changeFlag, flag,play,map=false,map1=false,show_allinfo,mapbool=false,mapmodel,mapdata} = this.props;
+        let {clickNumber, showFlag, changeFlag, flag,play,map=false,map1=false,show_allinfo,mapbool=false,mapmodel,mapdata,enterpage,enterpage1} = this.props;
         
         
         if(mapbool){
+            console.log(mapmodel)
+             
         let wind=[];
         let light=[];
         let wTact=0,lTact=0;
@@ -49,7 +51,7 @@ let Component = React.createClass({
             })
         }
         }
-        console.log(wTact)
+      
         
         return (
             <div className={styles.bodyBox}>
@@ -60,6 +62,52 @@ let Component = React.createClass({
                 <div className={styles.rightBox}>
                     <div className={`${styles.bigimg} ${map===true? styles.animat4 : styles.animat6} ${map1===true? styles.animat5 : styles.animat6} `}>
                         <div className={map==true? styles.place1 : styles.place2}  onClick={()=>play(map,map1)}></div>
+                        <div id='sonitem'>
+                             <div className={styles.mapenter} style={{top:'170px',left:'319px'}} onClick={()=>enterpage1('150811',1)}></div>
+                            <div className={styles.mapenter} style={{top:'183px',left:'219px'}} onClick={()=>enterpage('150801',0)}></div>
+                            <div className={styles.mapenter} style={{top:'359px',left:'219px'}} onClick={()=>enterpage1('150803',0)}></div>
+                            <div className={styles.mapenter} style={{top:'453px',left:'419px'}} onClick={()=>enterpage1('150812',2)}></div>
+                            <div className={styles.mapenter} style={{top:'294px',left:'77px'}} onClick={()=>enterpage('150828',1)}></div>
+                        </div>
+                        <div id='soninfo'>
+
+                            <div className={styles.soninfo} style={{top:'0px',left:'300px'}}>
+                                <div className={styles.sontit}><a className={styles.anum1}>川井光伏电站</a><a className={styles.anum}><b>{Number((mapdata[150811].Capacity)*mapmodel.dis.Capacity.coeff).toFixed(mapmodel.dis.Capacity.place)}</b>{mapmodel.dis.Capacity.unit}</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.InverterCount.name}</a><a className={styles.anum}><b>{mapdata[150811].InverterCount}</b>台</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.PVTSI_Aver.name}</a><a className={styles.anum}><b>{mapdata[150811].PVTSI_Aver ==='null' ? '--':Number(mapdata[150811].PVTSI_Aver).toFixed(2)}</b>W/㎡</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>{mapdata[150811].TActPower ==='' ? '--':Number(mapdata[150811].TActPower).toFixed(2)}</b>kW</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>出力率</a><a className={styles.anum}><b>{mapdata[150811].TActPower/mapdata[150811].Capacity}</b>%</a></div>
+                            </div>
+                            <div className={`${styles.soninfo} ${styles.soninfo2}`} style={{top:'40px',left:'20px'}}>
+                                 
+                                 <div className={styles.sontit}><a className={styles.anum1}>川井风电场</a><a className={styles.anum}><b>{Number((mapdata[150801].Capacity)*mapmodel.dis.Capacity.coeff).toFixed(mapmodel.dis.Capacity.place)}</b>{mapmodel.dis.Capacity.unit}</a></div>
+                                 <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.WTCount.name}</a><a className={styles.anum}><b>{mapdata[150801].WTCount}</b>台</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.WindSpeed_DevAverValue.name}</a><a className={styles.anum}><b>{mapdata[150801].WindSpeed_DevAverValue ==='null' ? '--':Number(mapdata[150801].WindSpeed_DevAverValue).toFixed(2)}</b>m/s</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>{mapdata[150801].TActPower ==='' ? '--':Number(mapdata[150801].TActPower).toFixed(2)}</b>kW</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>出力率</a><a className={styles.anum}><b>{mapdata[150801].TActPower/mapdata[150801].Capacity}</b>%</a></div>
+                            </div>
+                            <div className={`${styles.soninfo} ${styles.soninfo3}`} style={{top:'285px',left:'360px'}}>
+                                 <div className={styles.sontit}><a className={styles.anum1}>道勒光伏电站</a><a className={styles.anum}><b>{Number((mapdata[150803].Capacity)*mapmodel.dis.Capacity.coeff).toFixed(mapmodel.dis.Capacity.place)}</b>{mapmodel.dis.Capacity.unit}</a></div>
+                                  <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.InverterCount.name}</a><a className={styles.anum}><b>{mapdata[150803].InverterCount}</b>台</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.PVTSI_Aver.name}</a><a className={styles.anum}><b>{mapdata[150803].PVTSI_Aver ==='null' ? '--':Number(mapdata[150803].PVTSI_Aver).toFixed(2)}</b>W/㎡</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>{mapdata[150803].TActPower ==='' ? '--':Number(mapdata[150803].TActPower).toFixed(2)}</b>kW</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>出力率</a><a className={styles.anum}><b>{mapdata[150803].TActPower/mapdata[150803].Capacity}</b>%</a></div>
+                            </div>
+                            <div className={`${styles.soninfo} ${styles.soninfo4}`} style={{top:'465px',left:'520px'}}>
+                                 <div className={styles.sontit}><a className={styles.anum1}>乌漫光伏电站</a><a className={styles.anum}><b>{Number((mapdata[150812].Capacity)*mapmodel.dis.Capacity.coeff).toFixed(mapmodel.dis.Capacity.place)}</b>{mapmodel.dis.Capacity.unit}</a></div>
+                                  <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.InverterCount.name}</a><a className={styles.anum}><b>{mapdata[150812].InverterCount}</b>台</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.PVTSI_Aver.name}</a><a className={styles.anum}><b>{mapdata[150812].PVTSI_Aver ==='null' ? '--':Number(mapdata[150812].PVTSI_Aver).toFixed(2)}</b>W/㎡</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>{mapdata[150812].TActPower ==='' ? '--':Number(mapdata[150812].TActPower).toFixed(2)}</b>kW</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>出力率</a><a className={styles.anum}><b>{mapdata[150812].TActPower/mapdata[150812].Capacity}</b>%</a></div>
+                            </div>
+                            <div className={`${styles.soninfo} ${styles.soninfo5}`} style={{top:'380px',left:'-20px'}}>
+                                <div className={styles.sontit}><a className={styles.anum1}>敖包风电场</a><a className={styles.anum}><b>{Number((mapdata[150828].Capacity)*mapmodel.dis.Capacity.coeff).toFixed(mapmodel.dis.Capacity.place)}</b>{mapmodel.dis.Capacity.unit}</a></div>
+                                 <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.WTCount.name}</a><a className={styles.anum}><b>{mapdata[150828].WTCount}</b>台</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>{mapmodel.dis.WindSpeed_DevAverValue.name}</a><a className={styles.anum}><b>{mapdata[150828].WindSpeed_DevAverValue ==='null' ? '--':Number(mapdata[150828].WindSpeed_DevAverValue).toFixed(2)}</b>m/s</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>有功功率</a><a className={styles.anum}><b>{mapdata[150828].TActPower ==='' ? '--':Number(mapdata[150828].TActPower).toFixed(2)}</b>kW</a></div>
+                                <div className={styles.sontit1}><a className={styles.anum1}>出力率</a><a className={styles.anum}><b>{mapdata[150828].TActPower/mapdata[150828].Capacity}</b>%</a></div>
+                            </div>
+                        </div>
                         <div className={styles.allinfo} id='allinfo'>
                             <h3>综合指标</h3>
                             <p className={styles.capday}>
@@ -90,7 +138,7 @@ let Component = React.createClass({
                         Item.mapitem.map((value,key)=>{
                             if(key==5){
                             return(
-                                 <div className={styles.item_btn} key={key} onClick={()=>show_allinfo(map1)}>
+                                 <div className={styles.item_btn} key={key} onClick={()=>show_allinfo(map)}>
                                     <img src={value.url}/>
                                     <span>{value.name}</span>
                                  </div>
@@ -136,6 +184,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         getdata:()=>{
+            dispatch(actions.setVars('mapbool', false)); 
+            dispatch(actions.setVars('map', false));
+             dispatch(actions.setVars('map1', false));
             TY.getModel("6C5002D3-1566-414a-8834-5077940C78E1", 8888800, "DataOverview", setData, "Screen", 0);
                 function setData(mapmodel){
                     if(mapmodel.Model==undefined){
@@ -151,8 +202,9 @@ const mapDispatchToProps = (dispatch) => {
                                     }else{
                                     dispatch(actions.appendObjs('mapdata', mapdata.ModelData));
                                     setTimeout(function(){
-                                       dispatch(actions.setVars('mapbool', true));  
-
+                                       dispatch(actions.setVars('mapbool', true)); 
+                                       document.getElementById('sonitem').style.display='none';
+                                        document.getElementById('soninfo').style.display='none';
                                    },100)
                                      
                                 }
@@ -173,6 +225,7 @@ const mapDispatchToProps = (dispatch) => {
             
         },
         init: () => {
+
             var obj = {
                 test:''
             }
@@ -181,11 +234,15 @@ const mapDispatchToProps = (dispatch) => {
                 if(map){
                     map=false;
                     map1=true;
+                    document.getElementById('soninfo').style.display='none';
+                    document.getElementById('sonitem').style.display='none';
                 }else{
                     map1=false;
                     map=true;
+                    document.getElementById('allinfo').style.display='none';
+                    document.getElementById('sonitem').style.display='block';
                 }
-                $('#allinfo').hide();
+                
              dispatch(actions.setVars('map', map));
              dispatch(actions.setVars('map1', map1));
             
@@ -200,13 +257,39 @@ const mapDispatchToProps = (dispatch) => {
             dispatch(actions.setVars('flagff', flag));
         },
         show_allinfo:(map1)=>{
-            
             if(map1){
                 
-                $('#allinfo').toggle();
+                 $('#allinfo').hide();
+                $('#soninfo').toggle();
             }else{
-                $('#allinfo').hide();
+               
+                $('#allinfo').toggle();
+                $('#soninfo').hide();
             }
+        },
+        enterpage:(value2,key)=>{
+           dispatch(actions.setVars('showPage', 'fan_matrix'));
+           dispatch(actions.setVars('numpage', 'fanmatrix'));
+           dispatch(actions.setVars('fan_page', 'allpage'));
+           dispatch(actions.setVars('actbt',key ));
+          dispatch(actions.setVars('actbtn',0));
+          dispatch(actions.setVars('actbt1','' ));
+           dispatch(actions.setVars('fc_info', value2));
+           dispatch(actions.setVars('valuepage', value2));
+           dispatch(actions.setVars('befor_page','distribution' ));
+           dispatch(actions.setVars('Changnav', 0));
+        },
+         enterpage1:(value4,key)=>{
+           dispatch(actions.setVars('showPage', 'fan_matrix'));
+           dispatch(actions.setVars('numpage', 'pvmatrix'));
+           dispatch(actions.setVars('fan_page', 'allpage'));
+           dispatch(actions.setVars('actbt1',key ));
+          dispatch(actions.setVars('actbtn',0));
+          dispatch(actions.setVars('actbt','' ));
+           dispatch(actions.setVars('fc_info', value4));
+           dispatch(actions.setVars('valuepage1', value4));
+           dispatch(actions.setVars('befor_page','distribution' ));
+           dispatch(actions.setVars('Changnav', 1));
         }
     };
 };
