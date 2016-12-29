f464d63f04150dcd6428273f97b912986e156ac3
ludi001 <897911163@qq.com>
2016-12-29 15:22:56 +0800

    ludi12295

---
+++ app/routes/pages/mainPage/comp/wfm/report/runCount.jsx
@@ -0,0 +1,36 @@
+//设备运行模式字统计用例规划
+import React from 'react';
+import {connect} from 'react-redux';
+import styles from './runCountStyle.scss';
+import Login from '../../../../../../components/common/Loading.jsx';
+
+var actions = require('redux/actions');
+
+let Component = React.createClass({
+    componentWillMount() {
+		this.props.ajax();
+    },
+    componentDidlMount() {
+		this.props.init();
+    },
+    render() {
+    	return(
+
+    	)
+    }
+});
+const mapStateToProps = (state) => {
+    return{
+        
+    }
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        ajax: () => {
+
+        },
+        init: () => {
+
+        },
+    }
+};
