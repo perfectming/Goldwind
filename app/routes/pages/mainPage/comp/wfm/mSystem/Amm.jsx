580dd1bf01fe700f5036d587ef2b78e655dfbd82
QieT <644684987@qq.com>
2016-12-29 19:18:12 +0800

    jy111

--- app/routes/pages/mainPage/comp/wfm/mSystem/Amm.jsx
+++ app/routes/pages/mainPage/comp/wfm/mSystem/Amm.jsx
@@ -13,26 +13,19 @@ import add from '../../../img/icon/tabAdd.png';
 import _ from 'lodash';
 import Login from '../../../../../../components/common/Loading.jsx';
 import Ambox from './boxAm.jsx';
-let soam='http://10.9.99.203:8080/soam';
+let soam='http://10.9.99.68:8080/soam';
 let tabaleData = require('./data');
-
+let arr=['id','name','loginname','password','phonecode','mailbox','logintype','dogcode','remark','roleids'];
 let Component = React.createClass({
     componentDidMount() {
         this.props.init();
     },
-    buttonAction (){
-        var tContent = this.refs.textContent5.value;
-        alert(tContent);
-        // 在这个下边获取这个时间段的数据就行了
-        // 然后去更新图表
-    },
     render() {
-        let {boxData,jump,deleData,addData,buttonAction, inputOnChange, onFocus,table, changeTableItem} = this.props;
-        let newData=[];
+        let {ids,addDate,deleDate,ammCount,boxData,jump,deleData,addData,buttonAction, inputOnChange, onFocus,table, changeTableItem} = this.props;
+        let newData={};
         let num=0;
-        let arr=['id','name','loginname','password','phonecode','mailbox','logintype','dogcode','remark','id'];
-        for(let i=0;i<tabaleData.ammData.header.length;i++){
-            newData.push('');
+        for(let i=0;i<arr.length-1;i++){
+            newData[arr[i]]='';
         }
         let comp=tabaleData.comps.from;
         if(table&&boxData){
@@ -46,7 +39,7 @@ let Component = React.createClass({
                                 return (
                                     <div className={styles.inputBox} key={key}>
                                         <span>{comp[key].valueName}</span>
-                                        <input ref={'textContent'+key} placeholder={value.content}
+                                        <input id={'textContent'+key} placeholder={value.content}
                                                onChange={(e)=>inputOnChange(e.target.value, value.id)}
                                                onFocus={()=>onFocus} style={{width:value.width}}/>
                                     </div>
@@ -54,7 +47,7 @@ let Component = React.createClass({
                             }else if (value.type === 'button') {
                                 return (
                                     <div className={styles.btnBox} key={key}>
-                                        <button onClick={this.buttonAction}>{value.title}</button>
+                                        <button onClick={()=>{buttonAction()}}>{value.title}</button>
                                     </div>
                                 )
                             }
@@ -67,7 +60,7 @@ let Component = React.createClass({
                 <div className={styles.tableBox}>
                     <div className={styles.tableHeaderBox}>
                         <div className={styles.tableHeaderItem}
-                             style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}>序号</div>
+                             style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}>序 号</div>
                         {
                             tabaleData.ammData.header.map((value, key)=> {
                                 return (
@@ -81,6 +74,7 @@ let Component = React.createClass({
                         {
                             table.data.pagedata.map((value, key)=> {
                                 num++;
+                                if(key<ammCount/1){
                                 return (
                                     <div className={key%2===0? styles.tableContentLine : styles.tableContentLine1} key={key}>
                                         <input className={styles.tableContentItem}
@@ -88,6 +82,15 @@ let Component = React.createClass({
                                                readOnly="true" value={num}/>
                                         {
                                             arr.map((valueC, keyC)=> {
+                                                if(keyC<2){
+                                                    return(
+                                                        <input className={styles.tableContentItem}
+                                                               style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
+                                                               key={keyC} contentEditable="true"
+                                                               onChange={(e)=>changeTableItem(e.target.value,table,key,keyC)}
+                                                               value={value[valueC]}/>
+                                                    )
+                                                }
                                                 if (keyC==3){
                                                     return(
                                                         <input className={styles.tableContentItem}
@@ -100,7 +103,7 @@ let Component = React.createClass({
                                                     return (
                                                         <input className={styles.tableContentItem} key={keyC}
                                                            style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
-                                                           onClick={()=>{jump(value[valueC])}}
+                                                           onClick={()=>{jump(value['id'])}}
                                                            type="button" value='设 置'/>
                                                     )
                                                 }else {
@@ -118,10 +121,52 @@ let Component = React.createClass({
                                             <img src={save} onClick={()=>alert("您保存的数据123为:" + JSON.stringify(table.content[key]))}/>
                                         </div>
                                         <div className={styles.tableContentItem} style={{width:(50/(tabaleData.ammData.header.length+2))+"%"}}>
-                                            <img src={del} onClick={(e)=>deleData(key)}/>
+                                            <img src={del} onClick={(e)=>deleData(key,value.id)}/>
                                         </div>
                                     </div>
-                                )
+                                )}else {
+                                    return (
+                                        <div className={key%2===0? styles.tableContentLine : styles.tableContentLine1} key={key}>
+                                            <input className={styles.tableContentItem}
+                                                   style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
+                                                   readOnly="true" value={num}/>
+                                            {
+                                                arr.map((valueC, keyC)=> {
+                                                    if (keyC==3){
+                                                        return(
+                                                            <input className={styles.tableContentItem}
+                                                                   style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
+                                                                   key={keyC} contentEditable="true"
+                                                                   onChange={(e)=>changeTableItem(e.target.value,table,key,keyC)}
+                                                                   value={value[valueC]} type="password"/>
+                                                        )
+                                                    }else if (keyC==tabaleData.ammData.header.length-1){
+                                                        return (
+                                                            <input className={styles.tableContentItem} key={keyC}
+                                                                   style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
+                                                                   onClick={()=>{jump(value['id'])}}
+                                                                   type="button" value='设置'/>
+                                                        )
+                                                    }else {
+                                                        return(
+                                                            <input className={styles.tableContentItem}
+                                                                   style={{width:(100/(tabaleData.ammData.header.length+2))+"%"}}
+                                                                   key={keyC} contentEditable="true"
+                                                                   onChange={(e)=>changeTableItem(e.target.value,table,key,keyC)}
+                                                                   value={value[valueC]}/>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                            <div className={styles.tableContentItem} style={{width:(50/(tabaleData.ammData.header.length+2))+"%"}}>
+                                                <img src={save} onClick={(e)=>addDate(key,ids)}/>
+                                            </div>
+                                            <div className={styles.tableContentItem} style={{width:(50/(tabaleData.ammData.header.length+2))+"%"}}>
+                                                <img src={del} onClick={(e)=>deleDate(key)}/>
+                                            </div>
+                                        </div>
+                                    )
+                                }
                             })
                         }
                     </div>
@@ -138,6 +183,8 @@ const mapStateToProps = (state) => {
     return {
         table: state.objs.tableContentAmm,
         boxData: state.objs.boxData,
+        ammCount:state.vars.ammCount,
+        ids:state.vars.roleIds,
     }
 };
 
@@ -151,6 +198,7 @@ const mapDispatchToProps = (dispatch) => {
                 dataType: 'json',//here,
                 success:function (data) {
                     dispatch(actions.setObjs('tableContentAmm', data));
+                    dispatch(actions.setVars('ammCount', data.data.pagedata.length));
                 },
                 error:function(){
                     console.log('获取数据失败')
@@ -161,6 +209,7 @@ const mapDispatchToProps = (dispatch) => {
                 type: 'post',
                 dataType: 'json',//here,
                 success:function (data) {
+                    console.log(data);
                     dispatch(actions.appendObjs('boxData', data));
                 },
                 error:function(){
@@ -169,9 +218,27 @@ const mapDispatchToProps = (dispatch) => {
             });
 
         },
+        buttonAction (){
+            var tContent = $('#textContent5')[0].value;
+            // 在这个下边获取这个时间段的数据就行了
+            $.ajax({
+                    url: soam+'/user/getAllUser',
+                    type: 'post',
+                    data:'pageSize='+pageSize+'&&page='+1+'&&username='+tContent,
+                    dataType: 'json',//here,
+                    success:function (data) {
+                        console.log(data);
+                        dispatch(actions.setObjs('tableContentAmm', data));
+                    },
+                    error:function(){
+                        console.log('获取数据失败')
+                    }
+            });
+            // 然后去更新图表
+        },
         changeTableItem: (value, table, i, j) => {
-            let tableV = _.clone(getState().objs.tableContent);
-            tableV.content[i][j] = value;
+            let tableV = _.clone(getState().objs.tableContentAmm);
+            tableV.data.pagedata[i][arr[j]] = value;
             dispatch(actions.setObjs('tableContentAmm', tableV));
         },
         jump: (id) => {
@@ -189,18 +256,83 @@ const mapDispatchToProps = (dispatch) => {
                 }
             });
         },
+        addDate:(li,ids)=>{
+            let tableV = _.clone(getState().objs.tableContentAmm);
+            let wfs=tableV.data.pagedata[li];
+            wfs['roleids']=ids;
+            let ddv=JSON.stringify(wfs);
+            console.log(wfs,ddv);
+            $.ajax({
+                url: soam+'/user/addUser?userinfo=data',
+                type: 'post',
+                data: ddv,
+                dataType: 'json',//here,
+                contentType:'application/json;charset=UTF-8',
+                success:function (data) {
+                    console.log(data);
+                },
+                error:function(){
+                    console.log('获取数据失败')
+                }
+            });
+            $.ajax({
+                url: soam+'/user/getAllUser',
+                type: 'post',
+                data:'pageSize='+11+'&&page='+1+'&&username=',
+                dataType: 'json',//here,
+                success:function (data) {
+                    dispatch(actions.setObjs('tableContentAmm', data));
+                    dispatch(actions.setVars('ammCount', data.data.pagedata.length));
+                },
+                error:function(){
+                    console.log('获取数据失败')
+                }
+            });
+        },
+        deleDate:(j) => {
+            let tableV = _.clone(getState().objs.tableContentAmm);
+            tableV.data.pagedata.splice(j,1);
+            dispatch(actions.setObjs('tableContent', tableV));
+        },
         inputOnChange:(value,id)=>{
 
         },
         addData:(i) => {
-            let tableV = _.clone(getState().objs.tableContent);
-            tableV.content.push(i);
+            let tableV = _.clone(getState().objs.tableContentAmm);
+            console.log(tableV);
+            tableV.data.pagedata.push(i);
             dispatch(actions.setObjs('tableContentAmm', tableV));
         },
-        deleData:(j) => {
-            let tableV = _.clone(getState().objs.tableContent);
-            tableV.content.splice(j,1);
+        deleData:(j,k) => {
+            let tableV = _.clone(getState().objs.tableContentAmm);
+            $.ajax({
+                url: soam+'/user/getByIDDeleteUser?id='+k,
+                type: 'post',
+                dataType: 'json',//here,
+                success:function (data) {
+                    console.log(data);
+                    dispatch(actions.appendObjs('boxData', data));
+                    $('#boxAm').parent().css('display','block');
+                },
+                error:function(){
+                    console.log('获取数据失败')
+                }
+            });
+            tableV.data.pagedata.splice(j,1);
             dispatch(actions.setObjs('tableContentAmm', tableV));
+            $.ajax({
+                url: soam+'/user/getAllUser',
+                type: 'post',
+                data:'pageSize='+11+'&&page='+1+'&&username=',
+                dataType: 'json',//here,
+                success:function (data) {
+                    dispatch(actions.setObjs('tableContentAmm', data));
+                    dispatch(actions.setVars('ammCount', data.data.pagedata.length));
+                },
+                error:function(){
+                    console.log('获取数据失败')
+                }
+            });
         }
     };
 };
