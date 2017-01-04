bdc26c8e0e43fc7e730c4a767bd60bd544a99416
jhp <1033326818@qq.com>
2017-01-04 20:02:23 +0800

    jhp023

--- app/routes/pages/mainPage/comp/wbi/dashboard/grouprole/group/Group.jsx
+++ app/routes/pages/mainPage/comp/wbi/dashboard/grouprole/group/Group.jsx
@@ -6,7 +6,7 @@ import Pie2 from '../PieTwo';//饼图组件
 import Login from '../../../../../../../../components/common/Loading.jsx';//加载跳转页面
 
 
-var ipUrl='10.9.100.48:8080';
+var ipUrl='10.68.100.32:8080';
 var actions = require('redux/actions');
 var $ =require("jQuery");
 
